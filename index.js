f09667c03b1ef4421a51d3387a7a48a45b835799
Petit Kero Pro <bigboss@hidoge.cn>
2018-02-02 21:00:46 +0800

    no message

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const requestIp = require('request-ip')
 const { send } = require('micro')
 
 const config = {filter: f => f.indexOf('foo') !== -1}
@@ -5,6 +6,8 @@ const config = {filter: f => f.indexOf('foo') !== -1}
 let match = require('fs-router')(__dirname + '/routes', config)
 
 module.exports = async (req, res) => {
+  console.log('ip', requestIp.getClientIp(req))
+  console.log('res', res.requests)
   let matched = match(req)
   if (matched) return await matched(req, res)
   send(res, 404, { error: 'Not found' })
