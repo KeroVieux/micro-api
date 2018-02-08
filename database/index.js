0eaba6150bad924ffd1e5995417b1a53755a022c
hidoge <308680@gmail.com>
2018-02-08 17:49:27 +0800

    no message

--- database/index.js
+++ database/index.js
@@ -1,7 +1,6 @@
-const Config = require('../argument.config')
 const mongoose = require('mongoose')
 
-mongoose.connect(Config.mongoServer)
+mongoose.connect('mongodb://admin:1qCu5fpL@server.hidoge.cn:27018/sandbox')
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
