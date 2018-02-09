7d333c65ec8d0efd107efda44d5238a7eb7eb9bd
hidoge <308680@gmail.com>
2018-02-09 17:27:40 +0800

    no message

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://admin:1qCu5fpL@server.hidoge.cn:27018/sandbox')
+mongoose.connect('mongodb://sandUser:1qCu5fpL@112.124.57.15:27018/sandbox')
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
