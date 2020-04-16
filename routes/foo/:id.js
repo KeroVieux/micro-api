01a87d7f7da5714d3a3605a6d9047a790509fa7f
小黄狗 <308680@gmail.com>
2020-04-17 01:21:35 +0800

    Update :id.js

--- routes/foo/:id.js
+++ routes/foo/:id.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
     code: 0,
     exception: null,
     data: {
-      createdUserName: 'admin', lastModifiedUserName: 'admin', id: '16b48888-6e20-4133-ba0f-5eeb092dad6b', parameterCode: 'aaa', parameterName: 'aaa', parameterValue: '//wizplant.wizplant.online:2007/Attachment/0ab831b8cc948e82f028a7562bae7646.jpg', remark: 'ffff', parentId: '00000000-0000-0000-0000-000000000000', version: 0, status: 0, createdTime: '2019-01-21T19:00:31', createdUser: 'bc4ba096-ac22-4f68-b43d-9499679ede7a', lastModifiedTime: '2019-01-21T19:00:31', lastModifiedUser: 'bc4ba096-ac22-4f68-b43d-9499679ede7a', parameterType: 5,
+      createdUserName: 'admin', lastModifiedUserName: 'admin', id: '16b48888-6e20-4133-ba0f-5eeb092dad6b', parameterCode: 'aaa', parameterName: 'aaa', parameterValue: '/Attachment/0ab831b8cc948e82f028a7562bae7646.jpg', remark: 'ffff', parentId: '00000000-0000-0000-0000-000000000000', version: 0, status: 0, createdTime: '2019-01-21T19:00:31', createdUser: 'bc4ba096-ac22-4f68-b43d-9499679ede7a', lastModifiedTime: '2019-01-21T19:00:31', lastModifiedUser: 'bc4ba096-ac22-4f68-b43d-9499679ede7a', parameterType: 5,
     },
   })
 }
